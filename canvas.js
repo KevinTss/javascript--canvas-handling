1498da0fbbc0501bc5058e0ffc77d3b59ef2981f
Kevin Tassi <kevin@wooclap.com>
2020-01-17 14:15:19 +0100

    Draw simple rectangle

--- canvas.js
+++ canvas.js
@@ -3,4 +3,18 @@ const canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let context = canvas.getContext("2d");
+
+function drawRectangle(
+  context,
+  positionFromX,
+  positionFromY,
+  rectSizeX,
+  rectSizeY
+) {
+  return context.fillRect(positionFromX, positionFromY, rectSizeX, rectSizeY);
+}
+
+drawRectangle(context, 10, 200, 500, 50);
+
 console.log(canvas);
